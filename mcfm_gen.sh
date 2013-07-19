405807a5b16bd69cd1900f92a65c0f094c913487
lchaparr <luisilla.fer@cern.ch>
2013-07-19 23:40:23 +0200

    script to create files

---
+++ mcfm_gen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd workspace
+source rootsetup.sh
+cd MCFM-6.6/Bin
+
+for i in {1089..1109};
+do
+  cp background.DAT background"_"$i.DAT
+  sed -i s/1089/$i/g background"_"$i.DAT
+  sed -i s/zeeg/$i/g background"_"$i.DAT
+  ./mcfm background"_"$i.DAT
+  
+done
