323c8f1729ee1a33d92a28ae27be882e2cabc7ab
lchaparr <luisilla.fer@cern.ch>
2013-07-22 14:13:06 +0200

    root

---
+++ rootsetup.sh
@@ -0,0 +1,6 @@
+source /afs/cern.ch/sw/lcg/contrib/gcc/4.3/x86_64-slc5-gcc43-opt/setup.sh
+export LD_LIBRARY_PATH=$PYTHONDIR/lib:$LD_LIBRARY_PATH
+export ROOTSYS=/afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.07_python2.7/x86_64-slc5-gcc43-opt/root
+export PATH=$ROOTSYS/bin:$PATH
+export LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=$ROOTSYS/pyroot:$ROOTSYS/lib:$PYTHONPATH
