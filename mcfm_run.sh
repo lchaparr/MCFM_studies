53be2b9c6a8e454095921107a1a6d7fa5fbdf878
lchaparr <luisilla.fer@cern.ch>
2013-07-22 17:28:01 +0200

    organized version

--- mcfm_run.sh
+++ mcfm_run.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
-cd workspace
+#!/bin/bash 
+cd $HOME/workspace
 source rootsetup.sh
 cd MCFM-6.6/Bin
+./mcfm ../../mcfm_production/zeej/DATfiles/backgroundj_1089.DAT
+mv Z_1jet_tota_cteq6_m_50__50__1089*.rz ../../mcfm_production/zeej/ROOTfiles
+cd $HOME/workspace/mcfm_production/zeej/ROOTfiles
 
-./mcfm background_1089.DAT
-done
 
--- mcfm_sub.sh
+++ mcfm_sub.sh
@@ -1,15 +1,7 @@
-
 #!/bin/bash
-cd workspace
-source rootsetup.sh
-cd MCFM-6.6/Bin
 
-for i in {1089..1089};
+for i in {1089..1289};
 do
-  cp background.DAT background"_"$i.DAT
-  sed -i s/1089/$i/g background"_"$i.DAT
-  sed -i s/zeeg/$i/g background"_"$i.DAT
-
   cp mcfm_run.sh mcfm_run"_"$i.sh
   sed -i s/1089/$i/g mcfm_run"_"$i.sh
   bsub -q 1nw mcfm_run"_"$i.sh
